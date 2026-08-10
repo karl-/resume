1d99e574d2012bdc5e03fbabb26af0003e174c2c
Karl Henkel <karlh07@gmail.com>
2026-08-10 14:25:28 -0400

    Add Email to contact section

--- src/Resume.cs
+++ src/Resume.cs
@@ -13,6 +13,7 @@ public class Contact
 {
     public string Name { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
     public string Website { get; set; } = string.Empty;
 }
